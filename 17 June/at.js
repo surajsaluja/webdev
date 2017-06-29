c8d90de0075ec51dee84bac7eff1845a8f7004f0
surajsaluja <surajdps13@gmail.com>
2017-06-30 01:06:06 +0530

    office-work

---
+++ 17 June/at.js
@@ -0,0 +1,10 @@
+function test(a, b) {
+    if (b === void 0) { b = 10; }
+    return a + b;
+}
+var Test = (function () {
+    function Test(x) {
+        this.x = x;
+    }
+    return Test;
+}());
---
+++ 17 June/at.ts
@@ -0,0 +1,10 @@
+function test(a,b=10){
+		return a+b;
+	}
+	
+	
+	class Test{
+		constructor(x){
+			this.x=x;
+		}
+	}
